01ebdc582706391d256777b2463e1c7323a17b2f
nichoth <nichoth@gmail.com>
2020-10-13 18:30:33 -0700

    compile basically works

--- build.js
+++ build.js
@@ -2,14 +2,17 @@ var hyperstream = require('hyperstream');
 var fs = require('fs');
 var marked = require('marked')
 
-fs.readFile(__dirname + '/src/resume.md', function (err, mdContent) {
+fs.readFile(__dirname + '/src/resume.md', 'utf8', (err, mdContent) => {
+    if (err) throw err
+
     var hs = hyperstream({
         'style': process.stdin,
         'body': {
             _appendHtml: marked(mdContent)
-        },
-        '#a': fs.createReadStream(__dirname + '/a.html'),
-        '#b': fs.createReadStream(__dirname + '/b.html')
+        }
     })
+
+    var rs = fs.createReadStream(__dirname + '/src/resume.html')
+    rs.pipe(hs).pipe(process.stdout)
 })
 
